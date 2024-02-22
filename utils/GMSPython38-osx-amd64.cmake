f7c63efea3aca9a091c35c765f826986cb363f8f
Olivier Huber <oli.huber@gmail.com>
2024-02-22 13:49:43 -0600

    python: add missing changes

--- utils/GMSPython38-osx-amd64.cmake
+++
@@ -1,5 +0,0 @@
-set(Python3_INCLUDE_DIRS /scratch/miniconda/3.8/include/python3.8/)
-set(Python3_NumPy_INCLUDE_DIRS /scratch/miniconda/3.8/lib/python3.8/site-packages/numpy/core/include)
-set(Python3_SOABI cpython-38-darwin)
-set(Python3_LINK_OPTIONS -undefined dynamic_lookup)
-set(Python3_platform "macosx_10_9_x86_64")
