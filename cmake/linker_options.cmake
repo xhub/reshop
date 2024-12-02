e18651f1ba3e86051be07f7b81abf90fd528bb28
Olivier Huber <oli.huber@gmail.com>
2024-12-02 13:40:12 -0600

    cmake: improve linker detection

--- cmake/linker_options.cmake
+++ cmake/linker_options.cmake
@@ -29,7 +29,14 @@
 
 set(LINUX_LINKER_COMMON_Z defs noexecstack now origin relro)
 # --error-execstack is not supported by lld
-set(LINUX_LINKER_COMMON   -Bsymbolic --build-id=uuid --gc-sections --warn-common --warn-execstack)
+set(LINUX_LINKER_COMMON       -Bsymbolic --build-id=uuid --gc-sections --warn-common --warn-execstack)
+set(LINUX_LINKER_COMMON_BFD   --warn-execstack --error-execstack)
+set(LINUX_LINKER_COMMON_LLD   --gdb-index)
+set(LINUX_LINKER_COMMON_MOLD  --gdb-index)
+
+set(LINKER_VERSION_REGEXP_BFD   "^GNU ld.* ([0-9]+\\.[0-9]+)")
+set(LINKER_VERSION_REGEXP_LLD   "^LLD ([0-9]+\\.[0-9]+)")
+set(LINKER_VERSION_REGEXP_MOLD  "^mold ([0-9]+\\.[0-9]+)")
 
 set(CMAKE_C_FLAGS_ASAN
   "${CMAKE_C_FLAGS_DEBUG} ${ASAN_COMPILE_FLAGS}" CACHE STRING
@@ -40,6 +47,36 @@ set(LINKER_FLAGS)
 # LINUX is only defined for cmake >= 3.25?
 if (CMAKE_SYSTEM_NAME MATCHES "Linux")
 
+   # Check if the linker is GNU ld
+   execute_process(
+    COMMAND ${CMAKE_LINKER} --version
+    OUTPUT_VARIABLE LINKER_VERSION_OUTPUT
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+   )
+
+   # Check if GNU ld is part of the version output
+   if (LINKER_VERSION_OUTPUT MATCHES "^GNU ld")
+      set(LINKER_FLAVOR "BFD")
+
+   elseif(LINKER_VERSION_OUTPUT MATCHES "^LLD")
+      set(LINKER_FLAVOR "LLD")
+   elseif(LINKER_VERSION_OUTPUT MATCHES "^mold")
+      set(LINKER_FLAVOR "MOLD")
+   else()
+      message(WARNING "Unknown linker from output of ${CMAKE_LINKER} --version:\n ${LINKER_VERSION_OUTPUT}")
+      return()
+   endif()
+
+   # Extract the version number
+   string(REGEX MATCH ${LINKER_VERSION_REGEXP_${LINKER_FLAVOR}} LINKER_VERSION_MATCH ${LINKER_VERSION_OUTPUT})
+   if (LINKER_VERSION_MATCH)
+      set(LINKER_VERSION "${CMAKE_MATCH_1}")
+   else()
+      message(WARNING "Could not determine GNU ld version from: ${LINKER_VERSION_OUTPUT}")
+   endif()
+   message(STATUS "Linker: ${LINKER_FLAVOR} version ${LINKER_VERSION} detected")
+
    foreach(_f ${LINUX_LINKER_COMMON})
       list(APPEND LINKER_FLAGS LINKER:${_f})
    endforeach()
@@ -48,9 +85,14 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
       list(APPEND LINKER_FLAGS LINKER:-z,${_z})
    endforeach()
 
-   if (CMAKE_LINKER IN_LIST "mold;ld.lld")
-      list(APPEND LINKER_FLAGS "LINKER:--gdb-index")
+   if (LINKER_FLAVOR IN_LIST "LLD;MOLD")
       add_compile_options(-ggnu-pubnames)
+   endif()
+
+   if (NOT LINKER_FLAVOR STREQUAL "BFD" OR LINKER_VERSION VERSION_GREATER_EQUAL "2.39")
+      foreach(_f ${LINUX_LINKER_COMMON_${LINKER_FLAVOR}})
+         list(APPEND LINKER_FLAGS LINKER:${_f})
+      endforeach()
    endif()
 
 elseif(APPLE)
