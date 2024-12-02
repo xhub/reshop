226a626a3d8554feb73ccbb8a7e46ee13a778dea
Olivier Huber <oli.huber@gmail.com>
2024-12-02 13:58:45 -0600

    cmake: fix issue for older cmake

--- cmake/linker_options.cmake
+++ cmake/linker_options.cmake
@@ -45,6 +45,7 @@ set(CMAKE_C_FLAGS_ASAN
 set(LINKER_FLAGS)
 
 # LINUX is only defined for cmake >= 3.25?
+# Todo: check for issues with crosscompilation
 if (CMAKE_SYSTEM_NAME MATCHES "Linux")
 
    # Check if the linker is GNU ld
@@ -93,6 +94,13 @@ if (CMAKE_SYSTEM_NAME MATCHES "Linux")
       foreach(_f ${LINUX_LINKER_COMMON_${LINKER_FLAVOR}})
          list(APPEND LINKER_FLAGS LINKER:${_f})
       endforeach()
+   endif()
+
+   set(CMAKE_LINKER_TYPE ${LINKER_FLAVOR})
+
+   if (CMAKE_VERSION VERSION_LESS 3.29)
+      string(TOLOWER ${LINKER_FLAVOR} COMPILER_LINKER_VALUE)
+      set(CMAKE_EXE_LINKER_FLAGS="-fuse-ld=${COMPILER_LINKER_VALUE}")
    endif()
 
 elseif(APPLE)
