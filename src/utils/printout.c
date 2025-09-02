cfb4958c468500edf121e8a01e97a87e776d03cc
Olivier Huber <oli.huber@gmail.com>
2025-09-01 20:03:37 -0500

    win: small improvement

--- src/utils/printout.c
+++ src/utils/printout.c
@@ -209,7 +209,7 @@ static void backtrace(void)
      {
          SymFromAddr( process, ( DWORD64 )( stack[ i ] ), 0, symbol );
 
-         printf( "%u %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
+         fprintf(stderr, "%u %s - 0x%0X\n", frames - i - 1, symbol->Name, symbol->Address );
      }
 
      free( symbol );
