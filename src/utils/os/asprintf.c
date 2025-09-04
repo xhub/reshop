4fa3f938f687978dca632e5cb639e4b8dfbd73d7
Olivier Huber <oli.huber@gmail.com>
2025-09-03 20:20:27 -0500

    asprintf: improve function definition

--- src/utils/os/asprintf.c
+++ src/utils/os/asprintf.c
@@ -2,27 +2,29 @@
 
 
 #if defined(_WIN32) && !defined(__CYGWIN__)
-#define vscprintf _vscprintf
 int asprintf(char **strp, const char *format, ...)
 {
     va_list ap;
     va_start(ap, format);
-    int len = vscprintf(format, ap);
+    int len = vsnprintf(NULL, 0, format, ap);
+    va_end(ap);
+
     if (len < 0) { return len; }
 
     len++;
     char *str = (char*)malloc((size_t) len);
     if (!str) { return -1; };
 
+    va_start(ap, format);
     int retval = vsnprintf(str, (size_t)len, format, ap);
+    va_end(ap);
 
-    if (retval == -1) {
+    if (retval < 0) {
         free(str);
-        return -1;
+        return retval;
     }
 
     *strp = str;
-    va_end(ap);
     return retval;
 }
 
