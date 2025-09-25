befbbddcba9895601cdbf4bb214b6e02a19b3b05
Olivier Huber <oli.huber@gmail.com>
2025-09-25 16:00:32 -0500

    backtrace: log on stderr

--- src/utils/macros.c
+++ src/utils/macros.c
@@ -8,7 +8,7 @@ void backtrace_(const char *expr, int status)
 {
    if (getenv("RHP_NO_BACKTRACE")) return;
 
-   printf("Call to %s failed!\tError is %s (%d)\n", expr, status_descr(status), status);
+   fprintf(stderr, "Call to %s failed!\tError is %s (%d)\n", expr, status_descr(status), status);
 
    if (!getenv("RHP_NO_STOP")) { GDB_STOP();} //NOLINT(bugprone-unused-return-value,cert-err33-c)
 
